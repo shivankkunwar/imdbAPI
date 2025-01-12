68c72167e39dfda9be8a3f90919679ffa9e48a52
Shivank Kunwar <shivankkunwar100@gmail.com>
2025-01-12 17:27:26 +0530

    movies added by defauly

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -1,51 +1,87 @@
 import Movie from '../models/Movie.js';
 import fetch from 'node-fetch';
 
-
+const popularMovieIds = [
+  'tt0111161', 'tt0068646', 'tt0071562', 'tt0468569', 'tt0050083', 'tt0108052',
+  'tt0167260', 'tt0110912', 'tt0060196', 'tt0120737', 'tt0109830', 'tt0137523',
+  'tt0080684', 'tt1375666', 'tt0167261', 'tt0073486', 'tt0099685', 'tt0133093',
+  'tt0047478', 'tt0114369'
+];
+const fetchOMDBMovies = async (ids) => {
+  const omdbMovies = await Promise.all(ids.map(async (id) => {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${id}`);
+    const data = await response.json();
+    return {
+      name: data.Title,
+      yearOfRelease: parseInt(data.Year),
+      poster: data.Poster,
+      plot: data.Plot,
+      isExternal: true,
+      externalId: data.imdbID
+    };
+  }));
+  return omdbMovies;
+};
 export const getMovies = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 20;
     const search = req.query.search || '';
 
-    const query = search ? {
-      name: { $regex: search, $options: 'i' }
-    } : {};
+    let query = {};
+    if (search) {
+      query = { name: { $regex: search, $options: 'i' } };
+    }
 
-    const movies = await Movie.find()
+    // Fetch local movies
+    const localMovies = await Movie.find(query)
       .populate('producer', 'name')
       .populate('actors', 'name')
-      .limit(limit)
-      .skip((page - 1) * limit)
-      .sort({ createdAt: -1 });
+      .sort({ createdAt: -1 })
+      .limit(limit);
 
-    const count = await Movie.countDocuments(query);
-    let externalMovies = [];
-    if (search) {
+    const localCount = await Movie.countDocuments(query);
+
+    let omdbMovies = [];
+    if (!search) {
+      // If no search query, use predefined list
+      omdbMovies = await fetchOMDBMovies(popularMovieIds);
+    } else {
+      // If there's a search query, search OMDB
       const omdbResponse = await fetch(
-        `http://www.omdbapi.com/?s=${search}&apikey=${process.env.OMDB_API_KEY}`
+        `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=${search}&type=movie&page=${page}`
       );
       const omdbData = await omdbResponse.json();
-      
       if (omdbData.Search) {
-        externalMovies = omdbData.Search.map(movie => ({
-          ...movie,
-          isExternal: true,
-          externalId: movie.imdbID
+        omdbMovies = await Promise.all(omdbData.Search.map(async (movie) => {
+          const detailResponse = await fetch(`http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${movie.imdbID}`);
+          const detailData = await detailResponse.json();
+          return {
+            name: detailData.Title,
+            yearOfRelease: parseInt(detailData.Year),
+            poster: detailData.Poster,
+            plot: detailData.Plot,
+            isExternal: true,
+            externalId: detailData.imdbID
+          };
         }));
       }
     }
 
+    const combinedMovies = [...localMovies, ...omdbMovies].slice(0, limit);
+    const totalCount = localCount + (search ? omdbMovies.length : popularMovieIds.length);
+
     res.json({
-      movies: [...movies, ...externalMovies],
+      data: combinedMovies,
       page,
-      pages: Math.ceil(count / limit),
-      total: count + externalMovies.length
+      pages: Math.ceil(totalCount / limit),
+      total: totalCount
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
 
 export const searchMovies = async (req, res) => {
     try {
